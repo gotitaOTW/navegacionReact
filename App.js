39af93a518879ce477c326e8a13fda81439812ac
Android Studio <devandroid@ort>
2025-05-23 12:14:44 -0300

    primero minutos de laburo

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
+//seguir chequeando el ejemplo de polsu:
+//git clone https://github.com/polshu/DAI-2022.git
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-PAPAFITA
-      </Text>
+      <MisTabs />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const stackHomeScreen = () =>{
+
+}
+
+const MisTabs = () => {
+  <Tab.navigator>
+    <Tab.Screen
+      name="HomeScreen"
+      component={stackHomeScreen}
+
+    />
+  </Tab.navigator>
+  
 }
 
 const styles = StyleSheet.create({
